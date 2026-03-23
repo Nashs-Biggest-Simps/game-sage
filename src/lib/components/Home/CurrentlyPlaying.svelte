8d885133f16e4fdf0f264f9c28bb7cb7bd6e9234
Aaron Meche <aaronmeche18@icloud.com>
2026-03-23 03:26:50 -0500

    currently playing in dashboard example done

--- src/lib/components/Home/CurrentlyPlaying.svelte
+++ src/lib/components/Home/CurrentlyPlaying.svelte
@@ -8,13 +8,17 @@
 <!--  -->
 
 <div class="wrapper">
-    <CurrentlyPlayingItem name="Persona S" message="45 Hours Played" />
-    <CurrentlyPlayingItem name="Dark Souls" message="23 Hours Played" />
-    <CurrentlyPlayingItem name="Half-Life 2" message="13 Hours Played" />
+    <CurrentlyPlayingItem name="Persona S" message="45 Hours Played" appID="1245620" />
+    <CurrentlyPlayingItem name="Dark Souls" message="23 Hours Played" appID="1091500" />
+    <CurrentlyPlayingItem name="Half-Life 2" message="13 Hours Played" appID="730" />
+    <CurrentlyPlayingItem name="Half-Life 2" message="13 Hours Played" appID="367520" />
 </div>
 
 <!--  -->
 
 <style>
-
+    .wrapper{
+        display: grid;
+        /* gap: 1.6rem; */
+    }
 </style>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -3,6 +3,7 @@
     import FilterStack from "$lib/components/Suggest/FilterStack.svelte";
     import GameGrid from "$lib/components/Suggest/GameGrid.svelte";
     import { app } from "$lib/firebase";
+    import CurrentlyPlaying from "$lib/components/Home/CurrentlyPlaying.svelte";
 
 </script>
 
@@ -12,6 +13,7 @@
     <div class="left-side">
         <div class="module">
             <div class="title">Currently Playing</div>
+            <CurrentlyPlaying />
         </div>
 
     </div>
@@ -31,17 +33,25 @@
     .page{
         display: grid;
         grid-template-columns: 3fr 2fr;
-        gap: 1.2rem;
-    }
-
-    .module{
-        padding: 1.6rem;
-        background: var(--lz0);
-        border-radius: 0.8rem;
+        gap: 1.6rem;
     }
 
     .right-side{
         display: grid;
-        gap: 1.2rem;
+        gap: 1.6rem;
+    }
+
+    .module{
+        background: var(--lb0);
+        padding: 1.6rem 0;
+        outline: solid 1pt var(--l3);
+        border-radius: 0.8rem;
+
+        .title{
+            padding: 1.6rem;
+            padding-top: 0;
+            font-size: 1.6rem;
+            font-weight: 700;
+        }
     }
 </style>
