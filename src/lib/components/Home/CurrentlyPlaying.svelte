736e8d3b21eef525927a182f7441e45b1ce20112
BoiGonMon <owencadewells@gmail.com>
2026-04-16 13:36:25 -0500

    made it actually get the 4 most recently played games on the dashboard

--- src/lib/components/Home/CurrentlyPlaying.svelte
+++ src/lib/components/Home/CurrentlyPlaying.svelte
@@ -1,18 +1,30 @@
 <script>
     import CurrentlyPlayingItem from "$lib/components/Home/CurrentlyPlayingItem.svelte";
+    import { steamAPI } from "$lib/steam.js";
+    import { onMount } from "svelte";
 
+    let recentGames = [];
 
-
+    onMount(() => {
+        steamAPI.getRecentlyPlayedGames((data) => {
+            recentGames = data.response.games;
+            console.log(recentGames);
+        });
+    });
 </script>
 
 <!--  -->
 
 <div class="wrapper">
-    <CurrentlyPlayingItem name="Elden Ring" message="45 Hours Played" appID="1245620" />
-    <CurrentlyPlayingItem name="Cyberpunk 2077" message="23 Hours Played" appID="1091500" />
-    <CurrentlyPlayingItem name="Counter-Strike 2" message="13 Hours Played" appID="730" />
-    <CurrentlyPlayingItem name="Fallout" message="13 Hours Played" appID="38400" />
+    {#each recentGames as game (game.appid)}
+        <CurrentlyPlayingItem 
+            name={game.name} 
+            message={`${Math.trunc(game.playtime_forever/60)} Hours Played`}
+            appID={game.appid} 
+        />
+    {/each}
 </div>
+
 
 <!--  -->
 
